7525e6efc316f5476cc69cca5387d1110f224324
samarth goyal <samarthagrawal1804@gmail.com>
2023-12-04 00:03:08 +0530

    adding components

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,5 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
-export {Header,Footer,Container,Logo,LogoutBtn}
+import RTE from "./RTE";
+export { Header, Footer, Container, Logo, LogoutBtn, RTE };
