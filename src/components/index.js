b27b4b3cf46163a952b57cec676c06cdd69bf96c
samarth goyal <samarthagrawal1804@gmail.com>
2023-12-04 01:38:10 +0530

    adding pages

--- src/components/index.js
+++ src/components/index.js
@@ -4,4 +4,25 @@ import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
-export { Header, Footer, Container, Logo, LogoutBtn, RTE };
+import SignUp from "./SignUp";
+import Login from "./Login";
+import Button from "./Button";
+import PostForm from "./PostForm/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import Select from "./Select";
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  RTE,
+  SignUp,
+  Login,
+  Button,
+  PostForm,
+  PostCard,
+  AuthLayout,
+  Select,
+};
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { SignUp as SignupConponent } from "../components";
+function Signup() {
+  return (
+    <div className="py-8">
+      <SignupConponent />
+    </div>
+  );
+}
+
+export default Signup;
